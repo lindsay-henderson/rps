798a3c8a4bd870c2c6d9ff57bbd90a75ce238194
LindsayH <lindsaynhenderson@gmail.com>
2022-12-27 14:24:01 -0600

    write comparison logic

--- app.js
+++ app.js
@@ -31,3 +31,23 @@ function play(event) {
   getPlayerChoice(event)
   console.log(playerChoice)
 }
+
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * (choices.length - 1))
+  compChoice = choices[randomIndex]
+}
+
+function compare() {
+  if (playerChoice === compChoice) {
+    msg = 'You tied!'
+  } else if (playerChoice === choices[0] && compChoice === choices[2]) {
+    msg = "Congrats! You win!"
+  } else if (playerChoice === choices[1] && compChoice === choices[0]) {
+    msg = "Congrats! You win!"
+  } else if (playerChoice === choices[2] && compChoice === choices[1]) {
+    msg = "Congrats! You win!"
+  } else {
+    msg = "You lose! Try again?"
+  }
+}
+
